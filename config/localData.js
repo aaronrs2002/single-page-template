c711f121d11e87b8879eace0ccc53e7628d269cf
Aaron Smith <aaron@web-presence.biz>
2025-12-12 14:01:25 -0700

    gathering and building json logic

--- config/localData.js
+++ config/localData.js
@@ -81,11 +81,25 @@ const data = [{
                 "img/dave4.jpg",
                 "img/dave3.jpg"]
             ,
-            "aboutText": "<p>My friend Dave Wilson was in a life altering accident in November 2014. A lady ran a stop sign and Dave's Life changed forever. Not just for Dave...who is now a quadriplegic but for Dave's precious daughter Ella Kate. And for David's entire family.</p><p>The medical expenses have piled up and Dave's family needs help.</p><p>Myself and a few others organize a yearly fundraiser. We will be hosting a Fundraising Golf Tournament at Duck Creek Golf Club Garland, TX Friday November 7th 9:00 AM and then at 7:00 PM that evening we will be hosting a fundraising happy hour at Drinks Saloon  in Richardson where we will be selling raffle tickets for prizes where the proceeds will go entirely to Dave.</p><p>Please RSVP if you will be attending one or both events.</p><p>We could use some help in getting raffle prizes.  So if you would like to help just let me know.</p><p>Look forward to seeing everyone again.</p><h4><u><a href=\"https://www.paypal.me/MarkBier?locale.x=en_US\" target=\"_blank\"> Donate: <i class=\"fab fa-paypal\"></i></a></u></h4><!--<h4><u><a href=\"https://mechanized-aesthetics.net/WILSON-FUNDRAISER/downloads/Dave2024.jpg\" target=\"_blank\"> Information Flyer: <i class=\"fas fa-info-circle\"></i></a></u></h4>-->",
+            "aboutText": "<p>My friend Dave Wilson was in a life altering accident in November 2014. A lady ran a stop sign and Dave's Life changed forever. Not just for Dave...who is now a quadriplegic but for Dave's precious daughter Ella Kate. And for David's entire family.</p><p>The medical expenses have piled up and Dave's family needs help.</p><p>Myself and a few others organize a yearly fundraiser. We will be hosting a Fundraising Golf Tournament at Duck Creek Golf Club Garland, TX Friday November 7th 9:00 AM and then at 7:00 PM that evening we will be hosting a fundraising happy hour at Drinks Saloon  in Richardson where we will be selling raffle tickets for prizes where the proceeds will go entirely to Dave.</p><p>Please RSVP if you will be attending one or both events.</p><p>We could use some help in getting raffle prizes.  So if you would like to help just let me know.</p><p>Look forward to seeing everyone again.</p><h4><u><a href=\"https://www.paypal.me/MarkBier?locale.x=en_US\" target=\"_blank\"> Donate: <i class=\"fab fa-paypal\"></i></a></u></h4>",
             "aboutTitle": "Dave"
         }]
 
     ,
+    info: [
+
+        {
+            section: "sectionOne", HTMLcontent: `<div class="container"> <div class="row equalize1"><div data-equalize="0" class="col-md-6"><h2>Hello Everyone</h2><p> We are excited to announce our 9th Annual Dave Wilson Fundraising Extravaganza is going to happen this year.  It will be on Friday November 7, 2025.</p><p>This fundraising event will be broken into 2 parts. Hopefully you can attend one or both! Just like the past, we will have a Golf Tournament that Friday morning that will be at Duck Creek Golf Club (official Berkner High School Golf Course) at 9:00 am along with a Happy Hour (which Dave will attend) at Drinks Saloon at 7:00 pm that same day.</p><p>Cost per person $100. Please pay early so we can guarantee you a spot.</p><p>You can Paypal the money to <u><a href='mailto:mbier@apollostaffing.com'>mbier@apollostaffing.com</a></u> or Venmo <u><a href='https://account.venmo.com/u/Mark-Bier' target='_blank'>@Mark-Bier.</a></u>.</p><hr/><p>Also we are taking donations to help Dave with his Rehab costs. Please <u><a href='https://www.paypal.me/MarkBier?locale.x=en_US' target='_blank'> Donate: <i class='fab fa-paypal'></i> Mark Bier</a></u> for all golf fees or donations to <u><a href='mailto:mbier@apollostaffing'>mbier@apollostaffing.com</a></u> or Venmo at <u><a href='https://account.venmo.com/u/Mark-Bier' target='_blank'>@Mark-Bier</a></u>.</p></div><div data-equalize="0" class="col-md-6"><h2>Duck Creek Golf Club Garland, TX</h2><iframe class='img-fluid centerAuto img-thumbnail mediaComponent' data-type='video'  src='https://www.youtube.com/embed/2KsAtf6VgHE' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div></div></div>`
+        },
+
+
+
+        {
+            section: "sectionTwo", HTMLcontent: `<div class="container"><div class="row equalize1"><div data-equalize="0" class="col-md-6"><h2>Drinks Saloon</h2><img src='img/drinksSaloon2022.jpg' style='border:0px; width:100%; height:auto;'  /></div><div data-equalize="0" class="col-md-6"><h2>Draft beer & cocktails</h2><p>The place for Darts in Dallas. 22 draft selections and full bar. Feat. 18 big screens for all sports.</p><ul class='list-unstyled'><li><u><a href='https://www.facebook.com/drinkssaloon94/' target='_blank'><i class='fab fa-facebook'></i> facebook.com/drinkssaloon94</a></u></li><li><u><a href='https://www.instagram.com/drinkssaloon/' target='_blank'><i class='fab fa-instagram'></i> instagram.com/drinkssaloon</a></u></li><li><u><a href='https://twitter.com/SaloonDrinks' target='_blank'> <i class='fab fa-twitter'></i> twitter.com/SaloonDrinks</a></u></li></ul><hr/><p>Please RSVP if you will be attending one or both events.</p><p>We could use some help in getting raffle prizes.  So if you would like to help just let me know.</p><p>Look forward to seeing everyone again.</p></div></div></div>`
+        }
+    ]
+
+
 }
 ];
 let activeBusiness = 0;
@@ -113,3 +127,17 @@ document.getElementById("teasers").innerHTML = teasersHTML;
 document.getElementById("mobileTeasers").innerHTML = mobileTeasersHTML;
 document.querySelector("[data-content='0']").innerHTML = data[activeBusiness].teasers[0].teaserTitle;
 document.querySelector(".contentTxt[data-scroll='0']").innerHTML = "<h1>" + data[activeBusiness].about[0].aboutTitle + "</h1>" + data[activeBusiness].about[0].aboutText;
+
+
+/* START INFO */
+let pgSections = [];
+let sectionTitles = [];
+let infoTarget = "";
+for (let i = 0; i < data[activeBusiness].info.length; i++) {
+    let infoIntro = "";
+    if (i === 0) {
+        infoIntro = `<div class="container-fluid"><div class="row"><div class="col-md-12"><h2 class="txtCenter" data-target="Info">Info</h2><hr></div></div></div>`;
+    }
+    document.querySelector(".target[data-section='" + data[activeBusiness].info[i].section + "']").innerHTML = infoIntro + data[activeBusiness].info[i].HTMLcontent;
+
+}
