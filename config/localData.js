790831aa8655e5168a0ec2c4610f5d8fef4df01e
Aaron Smith <aaron@web-presence.biz>
2025-12-15 14:56:24 -0700

    added footer

--- config/localData.js
+++ config/localData.js
@@ -118,8 +118,182 @@ const data = [{
     ],
 
 
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+
+    title: "Basset Hounds",
+    map1: "AIzaSyBxvGBPN_lRhoY",
+    map2: "skabk_lZ5FAo4GIowU6I",
+    contact: {
+        address: "Scottsdale, AZ 85260",
+        addressIcon: "fa-home",
+        phone: "480.455.5555",
+        phoneIcon: "fa-phone",
+        email: "aaron@web-presence.biz",
+        emailIcon: "fa-envelope"
+
+    },
+
+    social: [
+        {
+            title: "instagram",
+            href: "https://www.instagram.com/aaronrs2002/",
+            icon: "fa-instagram"
+
+        },
+        {
+            title: "linkedin",
+            href: "https://www.linkedin.com/in/aaronrs2002/",
+            icon: "fa-linkedin"
+
+        },
+        {
+            title: "youtube",
+            href: "https://www.youtube.com/@web-presence-developer",
+            icon: "fa-youtube"
+
+        },
+        {
+            title: "donate",
+            href: "https://www.paypal.me/MarkBier?locale.x=en_US",
+            icon: "fa-donate"
+
+        }
+    ],
+    "jumbotronHeading": "Our Hounds Through The Years",
+
+    "jumbotronImg": "https://lh3.googleusercontent.com/pw/ACtC-3dWShOMXYK791jVBgzkDfTpOGqdhpjLYpuictKOofYwB1kWmc7hBVPDrh40B0f6FIaqlpbEWSV4Z8o6Kc3bh-0h2sqTYuNAIiTd60sQrKx-mIBe4iKc1GR2UtKysbfHMBoa1ZvfGXi7Lz91aGqDw5UkGw=w713-h318-no?authuser=0",
+
+    "jumbotronLead": "I have grown up with Bassets all my life. Their friendly nature along with an inquisitive urge to explore always appealed to me. ",
+    "teasers": [
+        {
+            "focusTeaser": "Meet Our Family",
+            "id": 0,
+            "teaserImg": "https://lh3.googleusercontent.com/pw/ACtC-3dJ6BEWZtqIm2Nfb4Vk9v1OQr_sOpk5eFszdphUNfWeDJNgp0VVqD0tGcuVAWfGSXFhnIf8bYgqyNC8PFjZAUffP-DBBCN_jQ_Bs0EPSAeI_ZOQi58lLraQD_YpsVxUxmqtzeqmX94wJntUliwbLEIFcA=s798-no?authuser=0",
+            "teaserText": "Each of our dogs",
+            "teaserTitle": "Our Hounds"
+        },
+        {
+            "focusTeaser": "Find Out More!",
+            "id": 1,
+            "teaserImg": "https://lh3.googleusercontent.com/pw/ACtC-3eVwPWMRHD2DRbZeY3Fqc5GoN8McpTcUJNK4B31QQRA7S5iwB6MfKgwfTlV_78p9f6brtbkosHbqQwGgYq13UJhR4u3cVN_uVaoPrn-XCjr5dBwmMD43b-3Kr1Gj5mC1tw4mCU2wBhDf6F9ta-O8ZbN2A=s500-no?authuser=0",
+            "teaserText": "We are sold on Bassets",
+            "teaserTitle": "Our Story"
+        },
+        {
+            "focusTeaser": "Hound Events",
+            "id": 2,
+            "teaserImg": "https://lh3.googleusercontent.com/pw/ACtC-3cHm8TQs6-RORMKbOgOtnM16788H3yXKohtcAJx6uH96PuxIGcUb2VBHhC_DvuB8tVwtFrf921rtVxcD3wH3zg5FP1ZBt44cprYDRgs2-H3G-k3zoG3RYofRSGxYn4k-Nkkm0DJLtLj2ugIU4J6zLe4hw=s500-no?authuser=0",
+            "teaserText": "Basset Events",
+            "teaserTitle": "Come join us!"
+        },
+        {
+            "focusTeaser": "Give us a shout!",
+            "id": 3,
+            "teaserImg": "https://lh3.googleusercontent.com/pw/ACtC-3f7gXfARRUgnfuxVRdZkUMviU61MPGSV5tklDUKPK4PZ_wJy3m9bLlmD_38thwhTClYtLw6ULusj-kegO9Mc2FOezx1S8XrXBNZ5XHDUD8JJsz_BdzMDsjeGUExO22CmvCCIvPf6pYcIgEt-Ie_LfRFjg=s500-no?authuser=0",
+            "teaserText": "Tell us your story",
+            "teaserTitle": "Give us a shout!"
+        }
+    ],
+    "about": [
+
+        {
+            "media": [
+                "img/DaveAndDaughter1.jpg",
+                "img/dave2.jpg",
+                "img/dave4.jpg",
+                "img/dave3.jpg"]
+            ,
+            "aboutText": "<p>My friend Dave Wilson was in a life altering accident in November 2014. A lady ran a stop sign and Dave's Life changed forever. Not just for Dave...who is now a quadriplegic but for Dave's precious daughter Ella Kate. And for David's entire family.</p><p>The medical expenses have piled up and Dave's family needs help.</p><p>Myself and a few others organize a yearly fundraiser. We will be hosting a Fundraising Golf Tournament at Duck Creek Golf Club Garland, TX Friday November 7th 9:00 AM and then at 7:00 PM that evening we will be hosting a fundraising happy hour at Drinks Saloon  in Richardson where we will be selling raffle tickets for prizes where the proceeds will go entirely to Dave.</p><p>Please RSVP if you will be attending one or both events.</p><p>We could use some help in getting raffle prizes.  So if you would like to help just let me know.</p><p>Look forward to seeing everyone again.</p><h4><u><a href=\"https://www.paypal.me/MarkBier?locale.x=en_US\" target=\"_blank\"> Donate: <i class=\"fab fa-paypal\"></i></a></u></h4>",
+            "aboutTitle": "Dave"
+        }]
+
+    ,
+    info: [
+
+        {
+            section: "sectionOne", HTMLcontent: `<div class="container"> <div class="row equalize1"><div data-equalize="0" class="col-md-6"><h2>Hello Everyone</h2><p> We are excited to announce our 9th Annual Dave Wilson Fundraising Extravaganza is going to happen this year.  It will be on Friday November 7, 2025.</p><p>This fundraising event will be broken into 2 parts. Hopefully you can attend one or both! Just like the past, we will have a Golf Tournament that Friday morning that will be at Duck Creek Golf Club (official Berkner High School Golf Course) at 9:00 am along with a Happy Hour (which Dave will attend) at Drinks Saloon at 7:00 pm that same day.</p><p>Cost per person $100. Please pay early so we can guarantee you a spot.</p><p>You can Paypal the money to <u><a href='mailto:mbier@apollostaffing.com'>mbier@apollostaffing.com</a></u> or Venmo <u><a href='https://account.venmo.com/u/Mark-Bier' target='_blank'>@Mark-Bier.</a></u>.</p><hr/><p>Also we are taking donations to help Dave with his Rehab costs. Please <u><a href='https://www.paypal.me/MarkBier?locale.x=en_US' target='_blank'> Donate: <i class='fab fa-paypal'></i> Mark Bier</a></u> for all golf fees or donations to <u><a href='mailto:mbier@apollostaffing'>mbier@apollostaffing.com</a></u> or Venmo at <u><a href='https://account.venmo.com/u/Mark-Bier' target='_blank'>@Mark-Bier</a></u>.</p></div><div data-equalize="0" class="col-md-6"><h2>Duck Creek Golf Club Garland, TX</h2><iframe class='img-fluid centerAuto img-thumbnail mediaComponent' data-type='video'  src='https://www.youtube.com/embed/2KsAtf6VgHE' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div></div></div>`
+        },
+
+
+
+        {
+            section: "sectionTwo", HTMLcontent: `<div class="container"><div class="row equalize1"><div data-equalize="0" class="col-md-6"><h2>Drinks Saloon</h2><img src='img/drinksSaloon2022.jpg' style='border:0px; width:100%; height:auto;'  /></div><div data-equalize="0" class="col-md-6"><h2>Draft beer & cocktails</h2><p>The place for Darts in Dallas. 22 draft selections and full bar. Feat. 18 big screens for all sports.</p><ul class='list-unstyled'><li><u><a href='https://www.facebook.com/drinkssaloon94/' target='_blank'><i class='fab fa-facebook'></i> facebook.com/drinkssaloon94</a></u></li><li><u><a href='https://www.instagram.com/drinkssaloon/' target='_blank'><i class='fab fa-instagram'></i> instagram.com/drinkssaloon</a></u></li><li><u><a href='https://twitter.com/SaloonDrinks' target='_blank'> <i class='fab fa-twitter'></i> twitter.com/SaloonDrinks</a></u></li></ul><hr/><p>Please RSVP if you will be attending one or both events.</p><p>We could use some help in getting raffle prizes.  So if you would like to help just let me know.</p><p>Look forward to seeing everyone again.</p></div></div></div>`
+        }
+    ],
+    "events": [
+        {
+            "address": "2800 Diamond Oaks Dr. Garland, TX 75044",
+            "contact": "(214) 440-2229",
+            "dateTime": "9:00 am Shotgun Start Friday November 7th",
+            "details": "<p>Cost per person $100. Please pay early so we can guarantee you a spot. You can Paypal the money to <u><a href='mailto:mbier@apollostaffing.com'>mbier@apollostaffing.com</a></u> or Venmo <u><a href='https://account.venmo.com/u/Mark-Bier' target='_blank'>@Mark-Bier.</a></u>.</p><p>Please RSVP if you will be attending one or both events.</p><p>We could use some help in getting raffle prizes.  So if you would like to help just let me know.</p><p>Look forward to seeing everyone again.</p>",
+            "title": "Duck Creek Golf Club"
+        },
+        {
+            "address": "13440 T I Blvd #2, Dallas, TX 75243",
+            "contact": "(972) 783-9900",
+            "dateTime": "Friday, November 7th at 7:00 p.m.",
+            "details": "<p>Basically in Richardson close to Spring Valley and Greenville. Right behind TI. Also, we are taking donations to help Dave with his Rehab costs.  Please PayPal Mark Bier for all golf fees or donations to <u><a href='mailto:mbier@apollostaffing.com'>mbier@apollostaffing.com</a></u> or Venmo at <u><a href='https://account.venmo.com/u/Mark-Bier' target='_blank'>@Mark-Bier.</a></u></p><p>Please RSVP if you will be attending one or both events.</p><p>We could use some help in getting raffle prizes.  So if you would like to help just let me know.</p><p>Look forward to seeing everyone again.</p>",
+            "title": "Drinks Saloon"
+        }
+    ],
 }
 ];
+
+
+
+
+
+
+
 let activeBusiness = 0;
 
 let teasersHTML = "";
